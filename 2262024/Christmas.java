6ad58d9efea31dbffec74d372b71f69af351f482
samad13 <yusufabdulsamad93@gmail.com>
2024-06-23 09:44:52 +0100

    adding for day 22/6/2024

---
+++ 2262024/Christmas.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class Christmas{ 
+	public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);	
+	
+		for( int day = 1; day <= 12; day++){
+	
+	System.out.print("on the day ");
+		
+		switch(day){
+			case 1:
+			System.out.print("first ");
+			break;
+			case 2:
+			System.out.print("second ");
+			break;
+			case 3:
+			System.out.print("third ");
+			break;
+			case 4:
+			System.out.print("fourth ");
+			break;
+			case 5:
+			System.out.print("fifth ");
+			break;
+			case 6:
+			System.out.print("sixth ");
+			break;
+			case 7:
+			System.out.print("seventh ");
+			break;
+			case 8:
+			System.out.print("eighth ");
+			break;
+			case 9:
+			System.out.print("ninth ");
+			break;
+			case 10:
+			System.out.print("tenth ");
+			break;
+			case 11:
+			System.out.print("eleventh ");
+			break;
+			case 12:
+			System.out.print("twelfth");
+			break;
+	
+		}
+
+		System.out.println("day of Christmas, My true love sent to me");
+
+			switch(day){
+			case 12:
+			System.out.print("Twelve drummers drumming, ");
+			break;
+			case 11:
+			System.out.print("Eleven pipers piping ");
+			break;
+			case 10:
+			System.out.print("Ten lords a-leaping,");
+			break;
+			case 9:
+			System.out.print("Nine ladies dancing, ");
+			break;
+			case 8:
+			System.out.print("Eight maids a-milking, ");
+			break;
+			case 7:
+			System.out.print("Seven swans a-swimming, ");
+			break;
+			case 6:
+			System.out.print("Six geese a-laying,");
+			break;
+			case 5:
+			System.out.print("Five golden rings, ");
+			break;
+			case 4:
+			System.out.print("Four calling birds, ");
+			break;
+			case 3:
+			System.out.print("Three French hens,");
+			break;
+			case 2:
+			System.out.print("Two turtledoves, ");
+			break;
+			case 1:
+			System.out.print("A partridge in a pear tree");
+			break;
+
+
+
+}
+			System.out.println("day of Christmas, My true love sent to me");
+			//System.out.println();
+
+}
+	
+
+
+}
+}
---
+++ 2262024/Triangle.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Triangle{ 
+	public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);	
+	
+
+	String asterik1 = " ";
+	String asterik2 = " ";
+	String asterik3 = " ";
+	String asterik4 = " ";
+	
+	for (int i = 1; i <= 10; i++) {
+
+ 	asterik1 +="*";
+	System.out.println(asterik1);
+ 	 }
+
+	for (int i = 1; i <= 10; i++) {
+ 	asterik2 +="*";
+	System.out.println(asterik2);
+ 	 }
+
+	for (int i = 1; i <= 10; i++) {
+
+ 	asterik3 +="*";
+	System.out.println(asterik3);
+ 	 }
+
+	for (int i = 1; i <= 10; i++) {
+ 	asterik4 +="*";
+	System.out.println(asterik4);
+ 	 }
+	
+
+	
+
+
+}
+} 
