5f4b2b7dc6b2e1792b95f53fe668710ee9aea8e2
samad13 <yusufabdulsamad93@gmail.com>
2024-08-05 13:25:01 +0100

    two

--- gateOne/Account.java
+++ gateOne/Account.java
@@ -2,16 +2,89 @@ public class Account {
 
 private String firstName;
 private String lastName; 
+private String accountNumber; 
 private double balance;
 private String pin;
 
-public Account(String name, double balance,String firstName,  String lastName, String pin) { 
+public Account(String firstName, String lastName, String accountNumber, double balance, String pin) { 
 this.firstName = firstName; 
 this.lastName = lastName;
 this.pin = pin;
+this.accountNumber = accountNumber; 
 if (balance > 0.0) {  
 this.balance = balance; 
 	} 
 } 
+
+
+public void setFirstName(String firstName) { 
+ 	 this.firstName = firstName;
+	} 
+
+	public  String getFirstName() { 
+	 return firstName; 
+	} 
+
+	public void setLastName(String lastName) { 
+ 	this.lastName = lastName;
+	} 
+
+	public String getLastName() { 
+	 return lastName; 
+	} 
+	
+
+	public void setPin(String pin) { 
+ 	this.pin = pin;
+	} 
+
+	public String getPin() { 
+	 return "****"; 
+	}
+ 
+	public void setAccountNumber(String accountNumber) { 
+ 	 this.accountNumber = accountNumber;
+	} 
+
+	public  String getAccountNumber() { 
+	 return accountNumber; 
+	} 
+
+
+	public double getBalance() { 
+ 	return balance; 
+	} 
+
+
+	public void withdraw(double withdrawAmount) {
+        if (withdrawAmount > balance) {
+            System.out.println("wsithdrawal amount exceeded account balance.");
+        } else {
+            balance -= withdrawAmount;
+        }}
+
+	public void deposit(double depositAmount) {
+        if (depositAmount <= 0.0) {
+            System.out.println("deposit amount is not valid, input a valid deposit");
+        } else {
+            balance += depositAmount;
+        }
+    }
+
+	public void transfer(double transferAmount,String accountNumber ) {
+	    if ( transferAmount <= 0.0 ) {
+            System.out.println("deposit amount is not valid, input a valid deposit");
+        } else {
+            balance -=  transferAmount;
+        }
+    }
+
+
+
+
+
+
+
+
 
 }
